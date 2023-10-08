94e7133c3a0c43dddfcd1216622aab582a5cdd54
xiaoye-hhh <1318137184@qq.com>
2023-10-08 19:59:45 +0800

    实现功能1

--- src/main/java/com/example/test2/Test2Application.java
+++ src/main/java/com/example/test2/Test2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test2Application {
 
     public static void main(String[] args) {
+        // 方兴业开发功能1
         SpringApplication.run(Test2Application.class, args);
     }
 
